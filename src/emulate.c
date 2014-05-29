3e313d425707f8a1f226752fde429e96c0d89e56
The Steez <PekanMomodu@dyn1216-251.wlan.ic.ac.uk>
2014-05-29 19:05:36 +0100

    Macros reimplemented to aid redesigned register struct. Skeleton done for decode but actual decoded yet to be completed.

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,4 @@
-
- /*
+/*
  *  Created on: 22 May 2014
  *      Author: Group 31
  */
@@ -8,18 +7,21 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdint.h>
-#include <assert.h> 
 
-typedef int bool;
-#define true 1
-#define false 0
+//These macros make it easier to reference the registers, leading to prettier code.
+#define Rg(X)  ARMReg[(X)].reg
+#define SP_    ARMReg[13].reg
+#define LR_    ARMReg[14].reg
+#define PC_    ARMReg[15].reg
+#define CPSR_  ARMReg[16].reg
 
  //Since static const didn't work, this is an alternative 
 //which is safer than just writing the number.
-enum {REG = 17, MEM = 65536};
+enum {REG = 17, MEM = 16384};
+enum bool {F, T};
 
 /*typedef only gives name to the struct ARM_STATE so that 
-you don't have to right struct ARM_STATE when you initialise
+you don't have to write struct ARM_STATE when you initialise
 the struct. This leads you to only have to write State when 
 initialising*/
 
@@ -30,12 +32,12 @@ typedef struct ARM_REGISTER {
 } Register;
 
 //Creating a pointer to the ARM Machine main memory.
-uint8_t *memPtr;
+uint32_t *memPtr;
 
 //This method is called by the main method to initialise all the memory.
 void initMem() {
-  uint8_t *memory = (uint8_t *) calloc(MEM, 1);
-  memPtr = memory;
+  memPtr = (uint32_t *) calloc(MEM, sizeof(uint32_t));
+
 } 
 
 //Initialised registers to 0 and assigned each
@@ -71,54 +73,59 @@ void printBits1(uint32_t x) {
   }
   printf("\n "); }
 
-
-
 //Data Processing instruction opcodes.
-void and(uint32_t rn, uint32_t op2, uint32_t des) {
-  ARMReg[des].reg = ARMReg[rn].reg & op2;
+
+/*  These use macros defined earlier to make code easier to read.
+    Rg(x) is replaced with ' ARMReg[x].reg ' at compile time.
+    PC_,SD_,LR & CPSR_ are similarly replaced with their respective registers.
+*/
+
+
+void and(uint32_t rn, uint32_t op2, uint32_t rd) {
+  Rg(rd) = Rg(rn) & op2;
 }
 
-void eor(uint32_t rn, uint32_t op2, uint32_t des) {
-  ARMReg[des].reg = ARMReg[rn].reg ^ op2;
+void eor(uint32_t rn, uint32_t op2, uint32_t rd) {
+  Rg(rd) = Rg(rn) ^ op2;
 }
 
-void sub(uint32_t rn, uint32_t op2, uint32_t des) {
+void sub(uint32_t rn, uint32_t op2, uint32_t rd) {
   uint32_t one = 1;
-  ARMReg[des].reg = ARMReg[rn].reg + (~op2 + one);
+  Rg(rd) = Rg(rn) + (~op2 + one);
 }
 
-void rsb(uint32_t rn, uint32_t op2, uint32_t des) {
-  sub(op2, rn, des);
+void rsb(uint32_t rn, uint32_t op2, uint32_t rd) {
+  uint32_t one = 1;
+  Rg(rd) = op2 + (~Rg(rn) + one);
 }
 
-void add(uint32_t rn, uint32_t op2, uint32_t des) {
-  ARMReg[des].reg = ARMReg[rn].reg + op2;
+void add(uint32_t rn, uint32_t op2, uint32_t rd) {
+  Rg(rd) = Rg(rn) + op2;
 }
 
 uint32_t tst(uint32_t rn, uint32_t op2) {
-  uint32_t result = ARMReg[rn].reg & op2;
+  uint32_t result = Rg(rn) & op2;
   return result;
 }
 
 uint32_t teq(uint32_t rn, uint32_t op2) {
-  uint32_t result = ARMReg[rn].reg ^ op2;
+  uint32_t result = Rg(rn) ^ op2;
   return result;
 } 
 
 uint32_t cmp(uint32_t rn, uint32_t op2) {
   uint32_t one = 1;
-  uint32_t result = ARMReg[rn].reg + (~op2 + one);
+  uint32_t result = Rg(rn) + (~op2 + one);
   return result;
 }
 
-void orr(uint32_t rn, uint32_t op2, uint32_t des) {
-  ARMReg[des].reg = ARMReg[rn].reg | op2;
+void orr(uint32_t rn, uint32_t op2, uint32_t rd) {
+  Rg(rd) = Rg(rn) | op2;
 }
 
-void mov(uint32_t op2, uint32_t des) {
-  ARMReg[des].reg = op2;
+void mov(uint32_t op2, uint32_t rd) {
+  Rg(rd) = op2;
 }
-
 
 //Creates a mask given the first and last index.
 uint32_t masking(uint32_t inst, int left, int right) {
@@ -127,29 +134,6 @@ uint32_t masking(uint32_t inst, int left, int right) {
   inst &= result;
   inst >>= right;
   return inst;
-}
-
-//Function to check conditions
-bool checkCond(uint32_t instruction) {
-	uint32_t cond = masking(instruction, 31, 28 );
-	uint32_t cpsrCond = masking(ARMReg[16].reg, 31, 28);
-	 return (cond == cpsrCond) || cond == 14 ; //Code = 1110 or matches CPSR
-}
-
-//Helper method to change a specific bit
- uint32_t changebit(uint32_t i, int bit, int change) {
-
-	assert(change == 0 || change == 1);
-
-	uint32_t mask = 1 * pow(2, bit);
-
-	if (change == 1) {
-		i |= mask; 
-	} else if (change == 0) {
-		i &= ~mask;
-	} 
-
-	return i;
 }
 
 //Opcode and operation for when operand2 is not an immediate constant.
@@ -162,83 +146,127 @@ uint32_t lsr(uint32_t rm, uint32_t shift) {
 }
 
 //enumeration of all operation mnemonics for help with decoding and execution.
- enum mnemonic {  AND, EOR, SUB,
-                  RSB, ADD, TST,
-                  TEQ, CMP, ORR,
-                  MOV, MUL, MLA,
-                  LDR, STR, BEQ,
-                  BNE, BGE, BLT,
-                  BGT, BLE, B,
-                  LSL, ANDEQ };
+enum mnemonic {  AND, EOR, SUB,
+                 RSB, ADD, TST,
+                 TEQ, CMP, ORR,
+                 MOV, MUL, MLA,
+                 LDR, STR, BEQ,
+                 BNE, BGE, BLT,
+                 BGT, BLE, B,
+                 LSL, ANDEQ };
 
 //Struct representing decoded instruction.
-    struct decodedInstruction {
+struct decodedInstruction {
         enum mnemonic operation;
         uint32_t rd;
         uint32_t rn;
         uint32_t rm;
         uint32_t op2;
-        int pending;
-    } ;
-
+        uint32_t i;
+        uint32_t a;
+        uint32_t s;
+        uint32_t cond;
+        enum bool pending;
+} ;
+    
 //Struct representing fetched instruction.
-    struct fetchedInstruction {
+struct fetchedInstruction {
         uint32_t binaryInstruction;
-        int pending;
-    } ;
+        enum bool pending;
+} ;
+    
+struct decodedInstruction *decoded;
+struct fetchedInstruction *fetched;
+
+void initdf(void) {
+      decoded = malloc(sizeof(struct decodedInstruction));
+      fetched = malloc(sizeof(struct fetchedInstruction));    
+}
+
+
 
  
 
 //function to increment PC to next instruction address.
-void incrementPC(){
+void incrementPC(void){
 
-    ARMReg[15].reg += 1;//PC is a macro defined earlier
+    PC_ += 1;//PC_ is a macro defined earlier
 
 }
 
 
 //function to fetch the instruction at the current instruction address stored in PC.
-void fetchNextInstruction(struct fetchedInstruction fetched){
+void fetchNextInstruction(){
 
-    fetched.binaryInstruction = ARMReg[15].reg; //PC is a macro defined earlier.
-    fetched.pending = 1;
+    fetched->binaryInstruction = PC_; //PC_ is a macro defined earlier.
+    fetched->pending = T;
     
 }
  
-//function to execute decoded instructions then clear decoded instructions pending flag.
-void execute(struct decodedInstruction decoded){
+//function to execute decoded instructions and clear decoded instructions pending flag.
+void executeDecodedInstruction(){
 
-    enum mnemonic op = decoded.operation;
+
+    decoded->pending = F;
+    
+    enum mnemonic op = decoded->operation;
+    
     switch (op){
-        case AND : and(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case EOR : eor(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case SUB : sub(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case RSB : rsb(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case ADD : add(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case TST : tst(decoded.rn, decoded.op2);
-        break;
-        case TEQ : teq(decoded.rn, decoded.op2);
-        break;
-        case CMP : cmp(decoded.rn, decoded.op2);
-        break;
-        case ORR : orr(decoded.rn, decoded.op2, decoded.rd);
-        break;
-        case MOV : mov(decoded.op2, decoded.rd);
-        default:
-        break;
+    
+        case AND : and(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case EOR : eor(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case SUB : sub(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case RSB : rsb(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case ADD : add(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case TST : tst(decoded->rn, decoded->op2);
+                   break;
+        case TEQ : teq(decoded->rn, decoded->op2);
+                   break;
+        case CMP : cmp(decoded->rn, decoded->op2);
+                   break;
+        case ORR : orr(decoded->rn, decoded->op2, decoded->rd);
+                   break;
+        case MOV : mov(decoded->op2, decoded->rd);
+                   break;
+        default:   break;
   }
     
 }
 
-
+//Helper function to figure out the operation mnemonic from the 32-bit instruction.
+enum mnemonic mnemonicDecoder(void){
+    return AND;
+}
 
 //function to decoded a previously fetched instrcution.
-void decode(struct fetchedInstruction fetched){
+void decodeFetchedInstruction(){
+ 
+  decoded->operation = mnemonicDecoder();
+  
+  switch(decoded->operation){
+
+        case AND : ;
+        case EOR : ;
+        case SUB : ;
+        case RSB : ;
+        case ADD : ;
+        case TST : ;
+        case TEQ : ;
+        case CMP : ;
+        case ORR : ;
+        case MOV : ; break;
+        
+        default:   break;
+
+  }
+  
+  decoded->pending = T;
+  
 }
 
 
@@ -246,59 +274,62 @@ void decode(struct fetchedInstruction fetched){
  //Main Function
  int main(int argc, char **argv) {
 
-   initMem();
+     initMem();
+     initdf();
 
-	 /* file loading */
-	 FILE *file = fopen(argv[1], "rb");
+     /* file loading */
+     FILE *file = fopen(argv[1], "rb");
 
-	 if (file == NULL){
-		 perror("Error: Could not open file.");
-		 exit(EXIT_FAILURE);
-	 }
+     if (file == NULL){
+             perror( " Error: Could not open file. \n" );
+             exit(EXIT_FAILURE);
+     }
      
      
-     /* Calculate file size then from the size, the number of 32-bit instructions in the file */
+     /* Calculate file size and then from the size, the number of 32-bit instructions in the file */
      const int bytesPerInstruction = 4;
      fseek( file, 0, SEEK_END );
      int instructionsSize = ftell(file)/bytesPerInstruction;
      fseek( file, 0, SEEK_SET );
 
      //Load the binary data into the memory array.
-	 fread(memPtr, bytesPerInstruction, instructionsSize, file );
+     fread(memPtr, bytesPerInstruction, instructionsSize, file );
 
-	 /* file closing */
-	 fclose(file);
+     /* file closing */
+     fclose(file);
 
 
 
-    //Pipeline execution of instructions.
-    //Start with no instructions decoded and no instructions fetched.
-    struct fetchedInstruction fetched;
-    struct decodedInstruction decoded;
-    fetched.pending = 0;
-    decoded.pending = 0;    
-
+     //Pipeline execution of instructions.
     
-    //main pipeline loop.
-     while ( 1 ) {
+     //main pipeline loop.
+     while ( T ) { //Loops infinitely until halt instruction is called
 
 
 
-        if ( decoded.pending == 1 ) {
+        if ( decoded->pending == T ) {
 
-            execute(decoded);
+            executeDecodedInstruction();
+            decoded->pending = F;
 
         }
 
-        if ( fetched.pending == 1 ) {
+        if ( fetched->pending == T ) {
 
-            decode(fetched);
+            decodeFetchedInstruction();
+            fetched->pending = F;
             
         }
 
-        if (ARMReg[15].reg <  MEM) {
+        if (PC_ <  MEM) { // PC_ is a macro defined earlier.
 
-            fetchNextInstruction(fetched);
+            fetchNextInstruction();
+            fetched->pending = T;
+            
+        } else if (decoded->pending == F) {
+        
+            break;
+            
         }
 
 
@@ -308,8 +339,6 @@ void decode(struct fetchedInstruction fetched){
      }
      
 
-	 return EXIT_SUCCESS;
+         return EXIT_SUCCESS;
 
  }
-
-
