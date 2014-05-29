5831c52601a27abc32045bd90f9007024250ff5c
Noe Amellal <pixel13.doc.ic.ac.uk>
2014-05-29 15:15:00 +0100

    The modified (i.e. completed implementation in) emulate.c file and the updated test suite.

--- src/emulate.c
+++ src/emulate.c
@@ -9,22 +9,22 @@
 #include <math.h>
 #include <stdint.h>
 
-//This macro makes it easier to reference the PC, leading to prettier code.
+// This macro makes it easier to reference the PC, leading to prettier code.
 #define PC state.reg[15]
 
 const int NUMBER_OF_REGISTERS = 17; // Number of registers
-const int memSize = 16384; //maximum number of instructions that can be stored.
+const int memSize = 16384; // maximum number of instructions that can be stored.
 uint32_t zero = 0;
 
 
-//Struct representing the state of machine
+// Struct representing the state of machine
   struct arm_State {
  	uint32_t reg[17];
 	uint32_t memory[16384];	
  }  ;
 
 
-//enumeration of all operation mnemonics for help with decoding and execution.
+// enumeration of all operation mnemonics for help with decoding and execution.
  enum mnemonic {  AND, EOR, SUB,
                   RSB, ADD, TST,
                   TEQ, CMP, ORR,
@@ -34,7 +34,7 @@ uint32_t zero = 0;
                   BGT, BLE, B,
                   LSL, ANDEQ };
 
-//Struct representing decoded instruction.
+// Struct representing decoded instruction.
     struct decodedInstruction {
         enum mnemonic operation;
         uint32_t Rd;
@@ -44,34 +44,31 @@ uint32_t zero = 0;
         int pending;
     } ;
 
-//Struct representing fetched instruction.
+// Struct representing fetched instruction.
     struct fetchedInstruction {
         uint32_t binaryInstruction;
         int pending;
     } ;
 
                   
-//Helper method to write to register
+// Helper method to write to register
  void putinreg(struct arm_State state, uint32_t rd, uint32_t op2) {
  	state.reg[rd] = op2;	
  }
  
- uint32_t and(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
+ void and(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
  	state.reg[r] = rn & op2;
- 	return state.reg[r];
  }
 
 
 
 
- uint32_t eor(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
+ void eor(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
  	state.reg[r] = rn ^ op2;
- 	return state.reg[r];
  }
 
- uint32_t sub(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
+ void sub(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
  	state.reg[r] = rn - op2;
- 	return state.reg[r];
  }
 
  int *decToBinary(int i) {
@@ -87,39 +84,38 @@ uint32_t zero = 0;
 	 return p;
  } 
 
- uint32_t rsb(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
+ void rsb(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
  	state.reg[r] = op2 - rn;
- 	return state.reg[r];
  }
 
 
- uint32_t add(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
+ void add(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
  	state.reg[r] = rn + op2;
- 	return state.reg[r];
  }
 
- void tst(struct arm_State state, uint32_t rn, uint32_t op2, int r) {
-	 state.reg[r] = state.reg[rn] & op2;
+ uint32_t tst(struct arm_State state, uint32_t rn, uint32_t op2) {
+	 // As AND but result not written
+	 return state.reg[rn] & op2;
  }
 
 
-//Helper method to fetch from register
+// Helper method to fetch from register
  uint32_t fetchfromreg(struct arm_State state, uint32_t rn) {
 	return state.reg[rn];
  }
 
-//Helper method for selecting bits
+// Helper method for selecting bits
  uint32_t selectbits(uint32_t i, int first, int last) {
 	uint32_t mask = 0;
 	int count = first;
 
-        //Create the 32 bit mask
+        // Create the 32 bit mask
 	while (count>=last) {
 		mask = mask + 1  * pow(2, count);
 		count--;
 	}
         
-	//& operation to extract the required bits
+	// & operation to extract the required bits
 	i &= mask;
 
 	//Extract the bits by shift right by 'last' amount of bits
@@ -128,31 +124,18 @@ uint32_t zero = 0;
         return i;
  }
 
- 
+ uint32_t teq(struct arm_State state, uint32_t rn, uint32_t op2) {
+	 // As EOR but result not written
+	 return state.reg[rn] ^ op2;
+ }
 
- void cmp(struct arm_State state, uint32_t rn,  uint32_t op2) {
- 
+ uint32_t cmp(struct arm_State state, uint32_t rn,  uint32_t op2) {
 	 // As SUB but result not written
- 	 uint32_t op1 = fetchfromreg(state, rn);
- 	 uint32_t result = op1 - op2;
-	 uint32_t newbit = 0;
-         uint32_t CPSR = fetchfromreg(state, 15); //Fetch contents of reg 15 (CPSR)
-	 
-	 if (result<0 ) {
-	 	newbit = selectbits(CPSR, 31, 31) | 1; //Change first bit to 1
-	 } else {
-		newbit = selectbits(CPSR, 31, 31) & 0; //Change first bit to 0
-	}
-
-	putinreg(state, 15, newbit * pow(2, 31));
-
+ 	 return state.reg[rn] - op2;
   }
 
  void orr(struct arm_State state, uint32_t rn, uint32_t op2, uint32_t rd) {
- 	uint32_t op1 = fetchfromreg(state, rn);
- 	uint32_t result = op1||op2; 
- 	putinreg(state, rd, result);
-
+	 state.reg[rd] = rn | op2;
   }
 
  void mov(struct arm_State state, uint32_t op2, uint32_t rd) {
@@ -160,7 +143,7 @@ uint32_t zero = 0;
  	putinreg(state, rd, op2);
  }
 
- //Multiply Instructions
+ // Multiply Instructions
 
 void multiply(struct arm_State state, uint32_t rs, uint32_t rm, uint32_t rd) {
 	uint32_t arg1 = fetchfromreg(state, rm);
@@ -173,15 +156,15 @@ void multiply(struct arm_State state, uint32_t rs, uint32_t rm, uint32_t rd) {
 
  
 
-//function to increment PC to next instruction address.
+// Method to increment PC to next instruction address.
 void incrementPC( struct arm_State state ){
 
-    PC += 1;//PC is a macro defined earlier
+    PC += 1;   // PC is a macro defined earlier
 
 }
 
 
-//function to fetch the instruction at the current instruction address stored in PC.
+// Method to fetch the instruction at the current instruction address stored in PC.
 void fetchNextInstruction(struct arm_State state, struct fetchedInstruction fetched){
 
     fetched.binaryInstruction = state.memory[ PC ]; //PC is a macro defined earlier.
@@ -189,7 +172,7 @@ void fetchNextInstruction(struct arm_State state, struct fetchedInstruction fetc
     
 }
  
-//function to execute decoded instructions then clear decoded instructions pending flag.
+// Method to execute decoded instructions then clear decoded instructions pending flag.
 void execute(struct arm_State state, struct decodedInstruction decoded){
 
     enum mnemonic op = decoded.operation;
@@ -202,22 +185,22 @@ void execute(struct arm_State state, struct decodedInstruction decoded){
 
 
 
-//function to decoded a previously fetched instrcution.
+// Method to decoded a previously fetched instrcution.
 void decode(struct fetchedInstruction fetched){
 }
 
 
 
- //Main Function
+ // Main Method
  int main(int argc, char **argv) {
 
-	//arm state initialised.
+	// arm state initialised.
 	struct arm_State ARM_State;
 
 
-	//Initialise the registers to zero
+	// Initialise the registers to zero
 	int i;
-	//Initialise the registers to zero
+	// Initialise the registers to zero
 	for (int i = 0; i < NUMBER_OF_REGISTERS; i++) {
 		ARM_State.reg[i] = zero;
 	}
@@ -245,7 +228,7 @@ void decode(struct fetchedInstruction fetched){
      int instructionsSize = ftell(file)/bytesPerInstruction;
      fseek( file, 0, SEEK_SET );
 
-     //Load the binary data into the memory array.
+     // Load the binary data into the memory array.
 	 fread(ARM_State.memory, bytesPerInstruction, instructionsSize, file );
 
 	 /* file closing */
@@ -255,15 +238,15 @@ void decode(struct fetchedInstruction fetched){
 	// just to make sure its working.
 	 printf("%d \n", instructionsSize);
 
-    //Pipeline execution of instructions.
-    //Start with no instructions decoded and no instructions fetched.
+    // Pipeline execution of instructions.
+    // Start with no instructions decoded and no instructions fetched.
     struct fetchedInstruction fetched;
     struct decodedInstruction decoded;
     fetched.pending = 0;
     decoded.pending = 0;    
 
     
-    //main pipeline loop.
+    // main pipeline loop.
      while ( 1 ) {
 
 	 }
