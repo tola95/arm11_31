6f950ee10fb232a91ce24be17f8f455ba5326c7b
Noe Amellal <corona22.doc.ic.ac.uk>
2014-06-03 19:40:19 +0100

    - Multiplying methods implemented with a suggestion of use in the main program  - branch file settled but interrupted by my plan changing suggestion  - suggestion for the implementation of a symbol table: we should implement all the body methods in the header file, so they would not interfere with the rest of the assembler code (i.e. like a helper structure).

--- src/multiplyer.c
+++ src/multiplyer.c
@@ -15,7 +15,12 @@
 
 
 
-
+/**
+ * We could make the main method scan the assembly instruction so that the
+ * number of the register specified is passed in as a parameter of each
+ * method below, and therefore it could be interpreted as a binary number which
+ * can be shifted as shown in their implementation below.
+ */
 
 uint32_t multiply(uint32_t rd, uint32_t rm, uint32_t rs) {
 	uint32_t result = (1110 << 28) + (1001 << 4) + (rd << 16) + (rs << 8) + rm;
